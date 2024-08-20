5dfe370cbe6a40cd0c8b2eff1ccffa07b668b6ea
Nagashree3007 <crnagashree8@gmail.com>
2024-08-20 12:46:26 +0530

    [Nagashree] Add:User need to follow pre-defined Mobile Format test case

--- test_case.py
+++ test_case.py
@@ -1,3 +1,14 @@
+'''
+
+@Author: Nagashree C R
+@Date: 2024-08-8-07
+@Last Modified by: Nagashree C R
+@Last Modified: 2024-08-07
+@Title :User registration problems UC4-User need to follow pre-defined Mobile Format 
+        - E.g. 91 9919819801 - Country code follow by space test case
+
+'''
+
 import unittest
 from use_registration import check_name
 from use_registration import check_mail
@@ -13,11 +24,12 @@ class TestCheckName(unittest.TestCase):
         self.assertEqual(check_name('aLice'), 0)
         
     def test_check_email_valid(self):
-        self.assertTrue(check_mail('abc.xyz@bl.co.in'))
-        self.assertTrue(check_mail('example@bl.co'))
-        self.assertTrue(check_mail('example@bl.co.in'))
-        self.assertFalse(check_mail('abc@xyz.co'))
-        self.assertFalse(check_mail('example@bl.com'))
+        self.assertTrue(check_mail('abc@1.com'))
+        self.assertTrue(check_mail('abc@gmail.com.com'))
+        self.assertTrue(check_mail('abc+100@gmail.com'))
+        self.assertFalse(check_mail('abc'))
+        self.assertFalse(check_mail('abc@.com.my'))
+        self.assertFalse(check_mail('abc123@gmail.a'))
         
     def test_check_phone_valid(self):
         self.assertTrue(check_phonenumber('+91 9919819801'))
